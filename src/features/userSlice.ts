7eb5cb38726c3a6d44e529aa1cc3f6d482770946
hiroto-hara <xre0605@gmail.com>
2022-08-06 15:58:47 +0900

    auth component

--- src/features/userSlice.ts
+++ src/features/userSlice.ts
@@ -1,24 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { RootState } from '../app/store';
+import { createSlice } from '@reduxjs/toolkit'
+import { RootState } from '../app/store'
 
 export const userSlice = createSlice({
   name: 'user',
-  initialState:{
-    user: {uid: "", photoUrl: "", displayName: ""}
+  initialState: {
+    user: { uid: '', photoUrl: '', displayName: '' },
   },
-  
+
   reducers: {
-    login: (state,action) => {
+    login: (state, action) => {
       state.user = action.payload
     },
     logout: (state) => {
-      state.user ={uid: "", photoUrl: "", displayName: ""}
+      state.user = { uid: '', photoUrl: '', displayName: '' }
     },
   },
-});
+})
 
-export const {login,logout} = userSlice.actions;
+export const { login, logout } = userSlice.actions
 
-export const selectUser = (state: RootState) => state.user.user;
+export const selectUser = (state: RootState) => state.user.user
 
-export default userSlice.reducer;
+export default userSlice.reducer
