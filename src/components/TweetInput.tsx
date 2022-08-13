846c68f5e01f56dccb14ce37815d631baa1b6f36
hiroto-hara <xre0605@gmail.com>
2022-08-13 16:06:27 +0900

    user avatar done

---
+++ src/components/TweetInput.tsx
@@ -0,0 +1,25 @@
+import { Avatar } from '@material-ui/core'
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectUser } from '../features/userSlice'
+import { auth } from '../firebase'
+
+import styles from './TweetInput.module.css'
+
+const TweetInput = () => {
+  const user = useSelector(selectUser)
+  console.log(user)
+  return (
+    <div>
+      <Avatar
+        className={styles.tweet_avatar}
+        src={user.photoUrl}
+        onClick={async () => {
+          await auth.signOut()
+        }}
+      />
+    </div>
+  )
+}
+
+export default TweetInput
