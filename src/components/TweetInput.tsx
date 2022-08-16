fb610e3c6e42d3e21da1cef12e8458bf4ba4c6d4
hiroto-hara <xre0605@gmail.com>
2022-08-16 18:39:43 +0900

    Feed display

--- src/components/TweetInput.tsx
+++ src/components/TweetInput.tsx
@@ -10,7 +10,7 @@ import styles from './TweetInput.module.css'
 import { Avatar, Button, IconButton } from '@material-ui/core'
 import AddAPhotoIcon from '@material-ui/icons/AddAPhoto'
 
-const TweetInput = () => {
+const TweetInput: React.FC = () => {
   const user = useSelector(selectUser)
   const [tweetImage, setTweetImage] = useState<File | null>(null)
   const [tweetMsg, setTweetMsg] = useState('')
